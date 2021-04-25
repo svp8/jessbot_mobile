7bbc9a63afd79022739c8ccdd0995a33b1992761
svp8 <azaz08082001@gmail.com>
2021-04-25 19:20:36 +0500

    te

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Navbar } from './src/Navbar';
 import { Content } from './src/Content'
-
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 
 export default function App() {
